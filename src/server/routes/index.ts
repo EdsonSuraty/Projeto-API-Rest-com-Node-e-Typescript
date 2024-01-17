d6f6bbb0148e5dbdc6f7d2763c44fa4eaa4ae0ff
Edson Suraty <edinhosuraty@gmail.com>
2024-01-17 18:13:54 -0300

    adiciona a base de uma controller e edita imports

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
+import { CidadesController } from "./../controllers/";
 
 const router = Router();
 
@@ -7,11 +8,7 @@ router.get("/", (_, res) =>{
     return res.end("ola");
 });
 
-router.post("/teste", (req,res)=>{
-    console.log(req.body);
-
-    return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
-});
+router.post("/cidades", CidadesController.create);
 
 
 
